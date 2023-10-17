04e5ef0fb25691f083ecf62b0984da4c2087b753
itz-amethyst <pransermi@gmail.com>
2023-10-17 16:43:46 +0330

    feat: Added basics of i18n 2/2

--- Home_Module/views.py
+++ Home_Module/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
 
 # Create your views here.
 
 
 def Index(request: HttpRequest):
-    return render(request, 'Home/Index.html', {})
+    context = {
+        'data': _('Hello World')
+    }
+    return render(request, 'Home/Index.html', context)
